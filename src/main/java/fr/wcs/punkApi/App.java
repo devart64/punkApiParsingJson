c4914e64ad525dcb473923cf8fa2cfa08e3c1463
devart64 <stephendupre64@gmail.com>
2018-04-15 07:02:17 +0200

    connexion a l'api avec un client http et lecture du json renvoyé

--- src/main/java/fr/wcs/punkApi/App.java
+++ src/main/java/fr/wcs/punkApi/App.java
@@ -1,13 +1,26 @@
 package fr.wcs.punkApi;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public  static OkHttpClient client = new OkHttpClient();
+    public static void main( String[] args ) throws IOException {
+
+        String result = run("https://api.punkapi.com/v2/beers");
+        System.out.println(result);
+
+
+    }
+
+    private static String run(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+
     }
 }
